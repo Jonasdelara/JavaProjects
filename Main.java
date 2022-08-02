ee30b03f760e157ea3c65985f2250f1e85bdf140
jonas de lara <78430135+Jonasdelara@users.noreply.github.com>
2022-08-02 16:17:28 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+     int idade;
+     double salario, altura;
+     char genero;
+     String nome;
+ 
+ idade = 30;
+ salario = 5800.5;	
+ altura = 1.72;
+ genero = 'f';
+ nome = "maria";
+ 
+ System.out.println("idade = " + idade);
+ System.out.println("salario = " + salario );
+ System.out.println("altura = " + altura );
+ System.out.println("genero = " + genero);
+ System.out.println("nome = " + nome);
+ 
+		 
+	}
+
+}
